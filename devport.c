04f6b0f8dca851878b25dd8c3e0651f46af06534
abond <abond@localhost.localdomain>
2024-06-27 14:24:50 -0600

    New changes, sorta works for voltages

--- devport.c
+++ devport.c
@@ -1,36 +1,56 @@
+#include <assert.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <sys/io.h>
 
-#define DEVPORT_UNLOCK 0x87
-#define DEVPORT_LOCK 0xaa
-#define DEVPORT_LDN 0x07
+#define ARR_SIZE(x) sizeof((x)) / sizeof((x[0]))
 
-uint8_t devport_read(uint16_t port)
+#define SIO_EFER 0x2e               /* Extended function enable register */
+#define SIO_EFDR SIO_EFER + 1       /* Extended function data register */
+
+#define SIO_UNLOCK 0x87
+#define SIO_LOCK 0xaa
+#define SIO_LDN_SEL 0x07
+
+uint8_t sio_read(uint8_t reg, uint16_t port)
 {
-    return inb_p(port);
+    outb_p(reg, port);
+    return inb_p(port + 1);
 }
 
-void devport_write(uint8_t value, uint16_t port)
+void sio_write(uint8_t value, uint8_t reg, uint16_t port)
 {
-    outb_p(value, port);
+    outb_p(reg, port);
+    outb_p(value, port + 1);
 }
 
-void devport_enter(void)
+void sio_enter(void)
 {
-    devport_write(DEVPORT_UNLOCK, 0x2e);
-    devport_write(DEVPORT_UNLOCK, 0x2e);
+    outb_p(SIO_UNLOCK, SIO_EFER);
+    outb_p(SIO_UNLOCK, SIO_EFER);
 }
 
-void devport_exit(void)
+void sio_exit(void)
 {
-    devport_write(DEVPORT_LOCK, 0x2e);
+    outb_p(SIO_LOCK, SIO_EFER);
 }
 
-void devport_select_ldn(uint8_t device)
+uint16_t sio_temperature_raw(uint8_t high, uint8_t low)
 {
-    devport_write(DEVPORT_LDN, 0x2e);
-    devport_write(device, 0x2f);
+    uint16_t base = high;
+    printf("base = %d\n", base);
+    return (base << 1) | low;
+}
+
+double sio_temperature(uint8_t high, uint8_t low)
+{
+    bool neg = high & 0x80;
+    int8_t base = ((high & 0x7f) << 1) | low;
+    if (neg)
+        printf("It's negative!\n");
+    printf("base = %d\n", base);
+    return base * 0.5;
 }
 
 int main(int argc, char **argv)
@@ -40,19 +60,42 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    int result = 0;
+    sio_enter();
 
-    devport_enter();
-    devport_select_ldn(0xb);
+    int chipid = (sio_read(0x20, SIO_EFER) << 8) | sio_read(0x21, SIO_EFER);
+    printf("chipid = 0x%x\n", chipid);
 
-    /*devport_write(0x20, 0x2e);*/
-    /*result = devport_read(0x2f);*/
+    /* Select hardware manager logical device. */
+    sio_write(0x0b, SIO_LDN_SEL, SIO_EFER);
 
-    devport_write(0x58, 0x2e);
-    result = devport_read(0x2f);
+    int iobase = (sio_read(0x60, SIO_EFER) << 8) | sio_read(0x61, SIO_EFER);
+    printf("iobase = 0x%x\n", iobase);
 
-    printf("result = 0x%x\n", result);
-    devport_exit();
+    int cr1a = sio_read(0x1a, SIO_EFER);
+    printf("cr1a = 0x%x\n", cr1a);
+    int cr1b= sio_read(0x1b, SIO_EFER);
+    printf("cr1b = 0x%x\n", cr1b);
+    int cr2a = sio_read(0x2a, SIO_EFER);
+    printf("cr2a = 0x%x\n", cr2a);
+    int cr2f = sio_read(0x2f, SIO_EFER);
+    printf("cr2f = 0x%x\n", cr2f);
+
+    /*int vendorid = sio_read(0xfe, iobase + 5);*/
+    /*printf("vendorid = 0x%x\n\n", vendorid);*/
+
+    printf("\n");
+    printf("CPUVCORE = %g\n", sio_read(0x00, iobase + 5) * 0.008);
+    printf("    VIN0 = %g\n", sio_read(0x01, iobase + 5) * 0.008);
+    printf("    AVSB = %g\n", sio_read(0x02, iobase + 5) * 0.016);
+    printf("    3VCC = %g\n", sio_read(0x03, iobase + 5) * 0.016);
+    printf("    VIN1 = %g\n", sio_read(0x04, iobase + 5) * 0.008);
+    printf("    VIN2 = %g\n", sio_read(0x05, iobase + 5) * 0.008);
+    printf("    VHIF = %g\n", sio_read(0x06, iobase + 5) * 0.016);
+    printf("    3VSB = %g\n", sio_read(0x07, iobase + 5) * 0.016);
+    printf("    VBAT = %g\n", sio_read(0x08, iobase + 5) * 0.016);
+    printf("     VTT = %g\n", sio_read(0x09, iobase + 5) * 0.008);
+
+    sio_exit();
 
     return 0;
 }
